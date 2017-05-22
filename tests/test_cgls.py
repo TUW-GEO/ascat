9f93414be1c2de3df27e0f73a393b859cfd61445
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2017-05-22 14:26:08 +0200

    Fix bug when reading QFLAG values of CGLS SWI product.

--- tests/test_cgls.py
+++ tests/test_cgls.py
@@ -43,5 +43,17 @@ def test_swi_ts_reader():
     assert len(data) == len(reference_index)
     assert np.all(data_sorted.index == reference_index)
 
+
+def test_swi_ts_qflag_reading():
+    data_path = os.path.join(
+        os.path.dirname(__file__), 'test-data', 'sat', 'cglops', 'swi_ts')
+    rd = SWI_TS(data_path, parameters=['SWI_001', 'QFLAG_001', 'SSF'])
+    data = rd.read_ts(3002621, mask_frozen=True)
+    # check if QFLAG is correctly read. It should have as many NaN values as
+    # SWI
+    assert len(data[data.loc[:, 'QFLAG_001'] != np.nan]) > 0
+    assert (len(data[data.loc[:, 'QFLAG_001'] == np.nan]) ==
+            len(data[data.loc[:, 'SWI_001'] == np.nan]))
+
 if __name__ == "__main__":
     test_swi_ts_reader()
