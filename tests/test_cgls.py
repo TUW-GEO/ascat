b032dc13b8d2f99358ef4d60e449c3d3855c47af
Sebastian Hahn <sebastian.hahn@gmail.com>
2017-07-27 15:15:12 +0200

    Update cgls tests

--- tests/test_cgls.py
+++ tests/test_cgls.py
@@ -22,12 +22,16 @@ import numpy as np
 
 
 def test_swi_ts_reader():
-
+    """
+    Test SWI time series reader.
+    """
     data_path = os.path.join(
-        os.path.dirname(__file__), 'test-data', 'sat', 'cglops', 'swi_ts')
+        os.path.dirname(__file__), 'test-data', 'cglops', 'swi_ts')
+
     rd = SWI_TS(data_path)
     data = rd.read_ts(3002621, mask_frozen=False)
     data_sorted = data.sort_index()
+
     assert np.all(data_sorted.index == data.index)
     # just check if enough data is there
     reference_index = pd.date_range('20070101T12:00:00', '20161231T12:00:00')
@@ -37,6 +41,7 @@ def test_swi_ts_reader():
     lon, lat = rd.grid.gpi2lonlat(3002621)
     data = rd.read_ts(lon, lat, mask_frozen=False)
     data_sorted = data.sort_index()
+
     assert np.all(data_sorted.index == data.index)
     # just check if enough data is there
     reference_index = pd.date_range('20070101T12:00:00', '20161231T12:00:00')
@@ -45,8 +50,11 @@ def test_swi_ts_reader():
 
 
 def test_swi_ts_qflag_reading():
+    """
+    Test SWI time series quality flag reader.
+    """
     data_path = os.path.join(
-        os.path.dirname(__file__), 'test-data', 'sat', 'cglops', 'swi_ts')
+        os.path.dirname(__file__), 'test-data', 'cglops', 'swi_ts')
     rd = SWI_TS(data_path, parameters=['SWI_001', 'QFLAG_001', 'SSF'])
     data = rd.read_ts(3002621, mask_frozen=True)
     # check if QFLAG is correctly read. It should have as many NaN values as
@@ -55,5 +63,7 @@ def test_swi_ts_qflag_reading():
     assert (len(data[data.loc[:, 'QFLAG_001'] == np.nan]) ==
             len(data[data.loc[:, 'SWI_001'] == np.nan]))
 
+
 if __name__ == "__main__":
     test_swi_ts_reader()
+    test_swi_ts_qflag_reading()
