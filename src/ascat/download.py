41633f797f8ab938b2eb07fb1d4eb4a2b3150417
Leonard Weydemann <lweydema@project10.geo.tuwien.ac.at>
2021-01-05 15:31:47 +0100

    adds basic functionality for eumetsat

--- src/ascat/download.py
+++ src/ascat/download.py
@@ -3,6 +3,10 @@ import sys
 from ftplib import FTP
 import getpass
 
+import re
+from datetime import datetime
+import requests
+
 #TODO: create general FTP and GET objects to pass queries to
 
 def parse_main_args_download(args):
@@ -21,12 +25,14 @@ def parse_main_args_download(args):
 def main_download(cli_args):
     args, parser = parse_main_args_download(cli_args)
     password = args.password#FIXME: getpass.getpass()
-    if args.source.upper() == 'EUMETSAT':
+    if args.source.upper() == 'HSAF':
         download_hsaf(username=args.username,
                      password=password,
                      product=args.product)
-    elif args.source.upper() == 'HSAF':
-        pass
+    elif args.source.upper() == 'EUMETSAT':
+        download_eumetsat(username=args.username,
+                          password=password,
+                          product=args.product)
 
 
 def download_hsaf(username, password, product):
@@ -42,8 +48,43 @@ def download_hsaf(username, password, product):
         ftp.cwd(path)
         import pdb;pdb.set_trace()
 
-def download_eumetsat(username, password):
-    pass
+def download_eumetsat(username, password, product):#TODO: date and productID to parse args
+    
+    access_token = 'TBD'
+    apis_endpoint = "http://api.eumetsat.int/"
+
+    service_search = apis_endpoint + "data/search-products/os"
+    service_download = apis_endpoint + "data/download/"
+
+    selected_collection_id = "EO:EUM:DAT:METOP:SOMO12"
+
+    coords = [[-1.0, -1.0],[4.0, -4.0],[8.0, -2.0],[9.0, 2.0],[6.0, 4.0],[1.0, 5.0],[-1.0, -1.0]]
+    start_date = datetime(2018, 12, 31)
+    end_date = datetime(2019, 1, 2)
+    #TODO: move search,product and collection to seperate methods
+    # Format our paramters for searching
+    dataset_parameters = {'format': 'json', 'pi': selected_collection_id}
+    dataset_parameters['dtstart'] = start_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    dataset_parameters['dtend'] = end_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    dataset_parameters['geo'] = 'POLYGON(({}))'.format(','.join(["{} {}".format(*coord) for coord in coords]))
+
+    # Retrieve datasets that match our filter
+    url = service_search
+    response = requests.get(url, dataset_parameters)
+    
+    found_data_sets = response.json()
+
+    for selected_data_set in found_data_sets['features']:
+        collID = selected_data_set['properties']['parentIdentifier']#FIXME: same as selected collid?
+        import pdb;pdb.set_trace()
+        
+    date = datetime.strptime(selected_data_set['properties']['date'].split("/",1)[0] , '%Y-%m-%dT%H:%M:%SZ')
+    download_url = service_download + urllib.parse.quote(
+            'collections/{collID}/dates/{year}/{month}/{day}/times/{hour}/{minute}'.format(
+            collID=collID, year=date.strftime('%Y'), 
+            month=date.strftime('%m'), 
+            day=date.strftime('%d'), hour=date.strftime('%H'), minute=date.strftime('%M')))
+        
 
 def run_download():
     main_download(sys.argv[1:])
