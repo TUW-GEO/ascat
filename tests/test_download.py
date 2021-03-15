ca28d12d6afcb19a40e772538b1dec1db02e589e
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2021-03-15 13:31:02 +0100

    Update environment.yml

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,247 +1,187 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 24 10:23:21 2021
+# Copyright (c) 2021, TU Wien, Department of Geodesy and Geoinformation
+# All rights reserved.
 
-@author: leonard
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of TU Wien, Department of Geodesy and Geoinformation
+#      nor the names of its contributors may be used to endorse or promote
+#      products derived from this software without specific prior written
+#      permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL TU WIEN DEPARTMENT OF GEODESY AND
+# GEOINFORMATION BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Tests for download.
 """
 
-import os
 import unittest
+import configparser
+from tempfile import mkdtemp
 from datetime import datetime, timedelta
 
-import numpy as np
+from ascat.download.connectors import HSAFConnector, EumetsatConnector
 
-import sys
-sys.path.insert(1,'../src/')
-
-from ascat.download_connectors import HSAFConnector, EumetsatConnector
-from ascat.download_interface import download_hsaf, download_eumetsat
-
-from tempfile import mkdtemp
-
-import configparser
 
 class TestDownload(unittest.TestCase):
-    
+
     def setUp(self):
-        
+
         self.credential_file = 'test_credentials.ini'
         yesterday = (datetime.now() - timedelta(1)).strftime('%Y%m%d')
         today = datetime.today().strftime('%Y%m%d')
         self.start_date = yesterday
         self.end_date = today
         self.path = mkdtemp()
-        
+
     def tearDown(self):
-        
+
         self.credential_file = None
         self.start_date = None
         self.end_date = None
-        
+
     def test_creds(self):
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
-        assert len(credentials.sections())>0
-        
+
+        assert len(credentials.sections()) > 0
+
     def test_eumetsat_connect(self):
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = EumetsatConnector()
         connector.connect(credentials=credentials)
-        
-    def test_eumetsat_download(self):  
-        
-        
+
+    def test_eumetsat_download(self):
+
         product = "EO:EUM:DAT:METOP:SOMO12"
-        coords = [[-1.0, -1.0],
-                  [4.0, -4.0],
-          		  [8.0, -2.0],
-          		  [9.0, 2.0],
-          		  [6.0, 4.0],
-          		  [1.0, 5.0],
-          		  [-1.0, -1.0]]
-        
+        coords = [[-1.0, -1.0], [4.0, -4.0], [8.0, -2.0],
+                  [9.0, 2.0], [6.0, 4.0], [1.0, 5.0], [-1.0, -1.0]]
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = EumetsatConnector()
         connector.connect(credentials=credentials)
-        
-        connector.download(product=product,
-                        download_dir=self.path,
-                        coords=coords,
-                        start_date=self.start_date,
-                        end_date=self.end_date,
-                        file_limit=1)
-    	
+
+        connector.download(product, self.path, coords, self.start_date,
+                           self.end_date, file_limit=1)
+
     def test_hsaf_connect(self):
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
+
         connector.close()
-        
+
     def test_hsaf_h08_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h08'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h08', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
+
     def test_hsaf_h10_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h10'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h10', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
+
     def test_hsaf_h16_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h16'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h16', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
+
     def test_hsaf_h101_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h101'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h101', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
+
     def test_hsaf_h102_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h102'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h102', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
+
     def test_hsaf_h103_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h103'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h103', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-    
+
     def test_hsaf_h104_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h104'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h104', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
+
     def test_hsaf_h105_download(self):
-        
-        
+
         credentials = configparser.ConfigParser()
         credentials.read(self.credential_file)
-        
+
         connector = HSAFConnector()
         connector.connect(credentials=credentials)
-        
-        product = 'h105'
-        
-        connector.download(product=product,
-                           download_dir=self.path,
-                           start_date=self.start_date,
-                           end_date=self.end_date,
+        connector.download('h105', self.path, self.start_date, self.end_date,
                            file_limit=1)
-        
         connector.close()
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
-        
