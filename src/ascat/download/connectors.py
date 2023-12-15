04f7e7214d1480c1d50e6b62d6433b01e40e3546
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2023-12-15 10:04:06 +0100

    update download module

--- src/ascat/download/connectors.py
+++ src/ascat/download/connectors.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, TU Wien, Department of Geodesy and Geoinformation
+# Copyright (c) 2023, TU Wien, Department of Geodesy and Geoinformation
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
@@ -24,17 +24,20 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+HTTP and FTP download module.
+"""
 
-import os
-import sys
 import base64
 import urllib
 import requests
 import logging
+from pathlib import Path, WindowsPath, PosixPath
 from ftplib import FTP
 from datetime import timedelta
+import concurrent.futures
 
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
@@ -42,7 +45,6 @@ logging.basicConfig(level=logging.INFO,
 
 
 class Connector:
-
     """
     Base class for connecting and downloading from remote source.
     """
@@ -69,7 +71,7 @@ class Connector:
         """
         pass
 
-    def download(self, path, start_date, end_date):
+    def download_date_range(self, path, start_date, end_date):
         """
         Fetch resource location for download of multiple files in date range.
 
@@ -84,7 +86,7 @@ class Connector:
         """
         pass
 
-    def grab_file(self, file_remote, file_local):
+    def download_file(self, file_remote, file_local):
         """
         Download single file from passed url to local file.
 
@@ -105,7 +107,6 @@ class Connector:
 
 
 class HttpConnector(Connector):
-
     """
     Class for http requests.
     """
@@ -121,7 +122,7 @@ class HttpConnector(Connector):
         """
         self.base_url = base_url
 
-    def grab_file(self, file_remote, file_local):
+    def download_file(self, file_remote, file_local, overwrite=False):
         """
         Download single file from passed url to local file.
 
@@ -131,43 +132,58 @@ class HttpConnector(Connector):
             Path of file to download
         file_local : string
             Path (local) where to save file
+        overwrite : bool, optional
+            If True, existing files will be overwritten.
         """
+        logging.debug("Send request")
         stream_response = requests.get(
-            file_remote, params={"format": "json"},
+            file_remote,
+            params={"format": "json"},
             stream=True,
-            headers={"Authorization": "Bearer {}".format(self.access_token)})
+            headers={"Authorization": f"Bearer {self.access_token}"})
 
         self._assert_response(stream_response)
+        logging.debug("Request received")
 
-        tail = ""
+        total = int(stream_response.headers["content-length"])
+
+        suffix = ""
         if stream_response.headers["Content-Type"] == "application/zip":
-            tail = ".zip"
+            suffix = ".zip"
 
-        # Download the file (and display progress)
-        progress = 0
+        filename = file_local.parent / (file_local.name + suffix)
 
-        logging.info("Start download: {}".format(file_local))
-
-        with open(file_local + tail, "wb") as f:
-            for chunk in stream_response.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    if progress % 1024 == 0:
-                        sys.stdout.write("\r%dkB" % progress)
-                        sys.stdout.flush()
-                    progress += 1
-            sys.stdout.write("\r%dkB" % progress)
-            sys.stdout.flush()
-
-        if os.path.exists(file_local + tail):
-            logging.info("Download finished")
+        if not overwrite and filename.exists():
+            lstat = filename.lstat()
+            if lstat.st_size == total:
+                logging.info("Skip download. File exits.")
         else:
-            logging.error("Downloaded file not found")
+            pbar = tqdm(desc=file_local.name,
+                        total=total,
+                        unit="B",
+                        unit_divisor=1024,
+                        unit_scale=True,
+                        leave=False)
+
+            with open(filename, "wb") as fp:
+                for chunk in stream_response.iter_content(chunk_size=1024):
+                    if chunk:
+                        fp.write(chunk)
+                        fp.flush()
+                        pbar.update(len(chunk))
+            pbar.close()
+
+            if filename.exists():
+                lstat = filename.lstat()
+                if lstat.st_size == total:
+                    logging.debug("Download successful")
+                else:
+                    logging.error("Download unsuccessful (file size mismatch)")
+            else:
+                logging.error("Downloaded file not found")
 
 
 class FtpConnector(Connector):
-
     """
     Class for downloading via FTP
     """
@@ -199,7 +215,7 @@ class FtpConnector(Connector):
         except:
             logging.error("FTP connection failed. User or password incorrect")
 
-    def grab_file(self, file_remote, file_local):
+    def download_file(self, file_remote, file_local, overwrite=False):
         """
         Download single file from passed url to local file
 
@@ -209,30 +225,58 @@ class FtpConnector(Connector):
             path of file to download
         file_local : string
             path (local) where to save file
+        overwrite : bool, optional
+            If True, existing files will be overwritten.
         """
         if file_remote not in self.ftp.nlst():
-            logging.warning(
-                "File not accessible on FTP: {}".format(file_remote))
+            logging.warning(f"File not accessible on FTP: {file_remote}")
         else:
-            localfile = open(file_local, "wb")
-            logging.info("Start download: {}".format(file_remote))
-            self.ftp.retrbinary("RETR " + file_remote, localfile.write, 1024)
-            localfile.close()
-            if os.path.exists(file_local):
-                logging.info("Finished download: {}".format(file_local))
+            logging.debug(f"Start download: {file_remote}")
+
+            total = self.ftp.size(file_remote)
+
+            if not overwrite and file_local.exists():
+                lstat = file_local.lstat()
+                if lstat.st_size == total:
+                    logging.info("Skip download. File exits.")
             else:
-                logging.error("Downloaded file not found")
+                pbar = tqdm(desc=file_local.name,
+                            total=total,
+                            unit="B",
+                            unit_divisor=1024,
+                            unit_scale=True,
+                            dynamic_ncols=True,
+                            leave=False)
+
+                with open(file_local, "wb") as fp:
+
+                    def cb(data):
+                        pbar.update(len(data))
+                        fp.write(data)
+
+                    self.ftp.retrbinary(f"RETR {file_remote}", cb)
+
+                pbar.close()
+
+                if file_local.exists():
+                    lstat = file_local.lstat()
+                    if lstat.st_size == total:
+                        logging.debug("Download successful")
+                    else:
+                        logging.error(
+                            "Download unsuccessful (file size mismatch)")
+                else:
+                    logging.error("Downloaded file not found")
 
     def close(self):
         """
         Close connection.
         """
         self.ftp.close()
-        logging.info("FTP disconnect")
+        logging.info("FTP disconnected")
 
 
 class HsafConnector(FtpConnector):
-
     """
     Class for downloading from HSAF via FTP.
     """
@@ -248,7 +292,13 @@ class HsafConnector(FtpConnector):
         """
         super().__init__(base_url)
 
-    def download(self, remote_path, local_path, start_date, end_date, limit=None):
+    def download(self,
+                 remote_path,
+                 local_path,
+                 start_date,
+                 end_date,
+                 limit=None,
+                 overwrite=False):
         """
         Fetch resource location for download of multiple files in date range.
 
@@ -264,15 +314,36 @@ class HsafConnector(FtpConnector):
             End date of date range interval.
         limit : int, optional
             Filter used to limit the returned results (default: 1).
+        overwrite : bool, optional
+            If True, existing files will be overwritten.
         """
+        download_url_list = []
+        local_file_list = []
+
         i = 0
         for daily_files in self.files(remote_path, start_date, end_date):
             for file_remote in daily_files:
-                file_local = os.path.join(local_path, file_remote)
-                self.grab_file(file_remote, file_local)
+                file_local = local_path / file_remote
+                local_file_list.append(file_local)
+                download_url_list.append(file_remote)
+
                 i = i + 1
                 if limit and limit == i:
                     break
+            else:
+                # continue if the inner loop wasn't broken
+                continue
+
+            # inner loop was broken, break the outer
+            break
+
+        with tqdm(desc="Downloads", total=len(download_url_list)) as pbar:
+            for download_url, local_file in zip(download_url_list,
+                                                local_file_list):
+                self.download_file(download_url,
+                                   local_file,
+                                   overwrite=overwrite)
+                pbar.update()
 
     def files(self, remote_path, start_date, end_date):
         """
@@ -300,15 +371,14 @@ class HsafConnector(FtpConnector):
         self.ftp.retrlines("NLST ", list_of_files.append)
 
         days = end_date - start_date
-        for i in tqdm(range(days.days)):
+        for i in range(days.days):
             date = (start_date + timedelta(days=i)).strftime("%Y%m%d")
-            matches = sorted(
-                [x for x in list_of_files if date in x], reverse=True)
+            matches = sorted([x for x in list_of_files if date in x],
+                             reverse=True)
             yield matches
 
 
 class EumConnector(HttpConnector):
-
     """
     Class for downloading from EUMETSAT via HTTP requests.
     """
@@ -337,8 +407,14 @@ class EumConnector(HttpConnector):
             consumer_key=credentials["consumer_key"],
             consumer_secret=credentials["consumer_secret"])
 
-    def download(self, product, local_path, start_date, end_date,
-                 coords=None, limit=None):
+    def download(self,
+                 product,
+                 local_path,
+                 start_date,
+                 end_date,
+                 max_workers=1,
+                 coords=None,
+                 limit=None):
         """
         Fetch resource location for download of multiple files in daterange.
 
@@ -354,25 +430,27 @@ class EumConnector(HttpConnector):
             Start date of date range interval.
         end_date : datetime
             End date of date range interval.
+        max_workers : int, optional
+            Number of parallel downloads (default: 1).
         coords : list of float, optional
             A custom polygon using EPSG:4326 decimal degrees (default: None).
         limit : int, optional
             Filter used to limit the returned results (default: None).
         """
-        service_search = self.base_url + "/data/search-products/os"
-        service_download = self.base_url + "/data/download/"
+        local_path = str2path(local_path)
+
+        service_search = f"{self.base_url}/data/search-products/os"
+        service_download = f"{self.base_url}/data/download/"
 
         dataset_parameters = {"format": "json", "pi": product}
 
-        dataset_parameters["dtstart"] = start_date.strftime(
-            "%Y-%m-%dT%H:%M:%S.%fZ")
-        dataset_parameters["dtend"] = end_date.strftime(
-            "%Y-%m-%dT%H:%M:%S.%fZ")
+        fmt = "%Y-%m-%dT%H:%M:%S.%fZ"
+        dataset_parameters["dtstart"] = start_date.strftime(fmt)
+        dataset_parameters["dtend"] = end_date.strftime(fmt)
 
         if coords:
-            dataset_parameters["geo"] = "POLYGON(({}))".format(
-                ",".join(["{} {}".format(*coord) for coord in coords])
-            )
+            dataset_parameters["geo"] = "POLYGON(({}))".format(",".join(
+                [f"{coord[0]} {coord[1]}" for coord in coords]))
 
         url = service_search
         response = requests.get(url, dataset_parameters)
@@ -392,40 +470,35 @@ class EumConnector(HttpConnector):
             response = requests.get(url, dataset_parameters)
             found_data_sets = response.json()
             all_found_data_sets.append(found_data_sets)
-            dataset_parameters['si'] = dataset_parameters['si'] + \
-                items_per_page
+            dataset_parameters[
+                'si'] = dataset_parameters['si'] + items_per_page
 
-        url_temp = "collections/{coll_id}/products/{product_id}"
+        download_url_list = []
+        local_file_list = []
 
-        stop_download = False
-
-        count = 0
         if all_found_data_sets:
-            print('Found {} data sets'.format(
-                found_data_sets['totalResults']))
 
             for found_data_sets in all_found_data_sets:
-                for selected_data_set in found_data_sets['features']:
-                    count = count + 1
-                    coll_id = selected_data_set["properties"]["parentIdentifier"]
-                    product_id = selected_data_set['properties']['identifier']
-                    download_url = service_download + urllib.parse.quote(
-                        url_temp.format(coll_id=coll_id, product_id=product_id))
-                    print(
-                        "Download URL (via product-ID) ({}): ".format(
-                            str(count)), download_url)
+                for selected_data_set in found_data_sets["features"]:
+                    coll_id = selected_data_set["properties"][
+                        "parentIdentifier"]
+                    product_id = selected_data_set["properties"]["identifier"]
+                    url_temp = f"collections/{coll_id}/products/{product_id}"
+                    download_url_list.append(service_download +
+                                             urllib.parse.quote(url_temp))
+                    local_file_list.append(local_path / product_id)
 
-                    file_local = os.path.join(local_path, product_id)
-                    self.grab_file(download_url, file_local)
+            print(f"Found {found_data_sets['totalResults']} data sets")
 
-                    if limit and limit == count:
-                        stop_download = True
-                        break
+            if limit and len(download_url_list) > limit:
+                print(f"Limited to {limit} data sets")
+                download_url_list = download_url_list[:limit]
+                local_file_list = local_file_list[:limit]
 
-                if stop_download:
-                    break
+            concurrent_download(self.download_file, download_url_list,
+                                local_file_list, max_workers)
         else:
-            print('No data sets found')
+            print("No data sets found")
 
     def _generate_token(self, consumer_key, consumer_secret):
         """
@@ -444,14 +517,15 @@ class EumConnector(HttpConnector):
         access_token : str
             An access token (if pass) or None (if fail).
         """
-        token_url = self.base_url + "/token"
-        userpass = consumer_key + ':' + consumer_secret
+        token_url = f"{self.base_url}/token"
+        userpass = f"{consumer_key}:{consumer_secret}"
         encoded_userpass = base64.b64encode(userpass.encode()).decode()
-        headers = {"Authorization": "Basic {}".format(encoded_userpass)}
+        headers = {"Authorization": f"Basic {encoded_userpass}"}
         data_payload = {"grant_type": "client_credentials"}
         response = requests.post(token_url, headers=headers, data=data_payload)
 
         self._assert_response(response)
+
         return response.json()["access_token"]
 
     def _assert_response(self, response, success_code=200):
@@ -471,6 +545,65 @@ class EumConnector(HttpConnector):
         result : None or str
             Nothing if success, error message if fail.
         """
-        assert (
-            response.status_code == success_code
-        ), "API Request Failed: {}\n{}".format(response.status_code, response.content)
+        msg = f"API Request Failed: {response.status_code}\n{response.content}"
+        assert (response.status_code == success_code), msg
+
+
+def concurrent_download(download_func,
+                        download_url_list,
+                        local_file_list,
+                        max_workers=1):
+    """
+    Threaded file download.
+
+    Parameters
+    ----------
+    download_func : function
+        Download function.
+    download_url_list : list
+        Download URLs.
+    local_file_list : list
+        Local filenames.
+    max_workers : int, optional
+        Number of concurrent downloads (default: 1).
+    """
+    if max_workers == 1:
+        with tqdm(desc="Downloads", total=len(download_url_list)) as pbar:
+            for download_url, local_file in zip(download_url_list,
+                                                local_file_list):
+                download_func(download_url, local_file)
+                pbar.update()
+    else:
+        with tqdm(desc="Downloads", total=len(download_url_list)) as pbar:
+            with concurrent.futures.ThreadPoolExecutor(
+                    max_workers=max_workers) as executor:
+
+                future_down = {
+                    executor.submit(download_func, url, local_file): url
+                    for url, local_file in zip(download_url_list,
+                                               local_file_list)
+                }
+
+                for future in concurrent.futures.as_completed(future_down):
+                    _ = future_down[future]
+                    pbar.update()
+
+
+def str2path(path):
+    """
+    Convert str path to pathlib.Path object.
+
+    Parameters
+    ----------
+    path : str, pathlib.Path
+        Path.
+
+    Returns
+    -------
+    path : pathlib.Path
+        Pathlib path.
+    """
+    if not isinstance(path, (WindowsPath, PosixPath)):
+        path = Path(path)
+
+    return path
