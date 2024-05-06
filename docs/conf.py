d8f078d5100b19055cfb2c5ab86b68725bbf9039
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2024-05-06 11:53:45 +0200

    update doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
-    "myst_nb",
+    # "myst_nb",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
