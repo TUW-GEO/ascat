743f71f9c636fa1c6ab40611218888708e57f600
Clay Harrison <clay.harrison@geo.tuwien.ac.at>
2024-12-03 16:05:32 +0100

    Refactor grid singleton to account for args

--- src/ascat/read_native/grid_registry.py
+++ src/ascat/read_native/grid_registry.py
@@ -1,28 +1,51 @@
+import inspect
 from fibgrid.realization import FibGrid
 
 
-class SingletonMeta(type):
+class Singleton(type):
+    """ Simple Singleton that keep only one value for all instances
     """
-    The Singleton class can be implemented in different ways in Python. Some
-    possible methods include: base class, decorator, metaclass. We will use the
-    metaclass because it is best suited for this purpose.
-    """
-
-    _instances = {}
+    def __init__(cls, name, bases, dic):
+        super(Singleton, cls).__init__(name, bases, dic)
+        cls.instance = None
 
     def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls.instance
 
-class GridSingleton(metaclass=SingletonMeta):
-    def __init__(self, grid_class, *args, **kwargs):
-        self.grid = grid_class(*args, **kwargs)
+
+class SingletonArgs(type):
+    """ Singleton that keep single instance for single set of arguments. E.g.:
+    assert SingletonArgs('spam') is not SingletonArgs('eggs')
+    assert SingletonArgs('spam') is SingletonArgs('spam')
+    """
+    _instances = {}
+    _init = {}
+
+    def __init__(cls, name, bases, dct):
+        cls._init[cls] = dct.get('__init__', None)
+
+    def __call__(cls, *args, **kwargs):
+        init = cls._init[cls]
+        if init is not None:
+            key = (cls, frozenset(
+                    inspect.getcallargs(init, None, *args, **kwargs).items()))
+        else:
+            key = cls
+
+        if key not in cls._instances:
+            cls._instances[key] = super(SingletonArgs, cls).__call__(*args, **kwargs)
+        return cls._instances[key]
+
+
+class GridSingleton(object):
+    """ Class based on Singleton type to work with MongoDB connections
+    """
+    __metaclass__ = SingletonArgs
+
+    def __init__(self, grid_class, arg1=None):
+        self.grid = grid_class(arg1)
 
 class GridRegistry:
     def __init__(self):
