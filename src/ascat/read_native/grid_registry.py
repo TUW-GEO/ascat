4ad179d654150be7c38638884a45f4f25b644981
Clay Harrison <clay.harrison@geo.tuwien.ac.at>
2024-12-04 10:21:30 +0100

    Refactor grid registry

--- src/ascat/read_native/grid_registry.py
+++ src/ascat/read_native/grid_registry.py
@@ -1,73 +1,52 @@
 import inspect
+from enum import Enum
+from typing import Dict, Type, Any, Tuple
+
 from fibgrid.realization import FibGrid
 from pygeogrids.netcdf import load_grid
 
 
-class Singleton(type):
-    """ Simple Singleton that keep only one value for all instances
-    """
-    def __init__(cls, name, bases, dic):
-        super(Singleton, cls).__init__(name, bases, dic)
-        cls.instance = None
-
-    def __call__(cls, *args, **kwargs):
-        if cls.instance is None:
-            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls.instance
+class GridType(Enum):
+    FIBGRID = "fibgrid"
+    NAMED = "named"
 
 
 class SingletonArgs(type):
-    """ Singleton that keep single instance for single set of arguments. E.g.:
-    assert SingletonArgs('spam') is not SingletonArgs('eggs')
-    assert SingletonArgs('spam') is SingletonArgs('spam')
-    """
-    _instances = {}
-    _init = {}
-
-    def __init__(cls, name, bases, dct):
-        cls._init[cls] = dct.get('__init__', None)
+    _instances: Dict[Tuple[Type, Tuple, frozenset], Any] = {}
 
     def __call__(cls, *args, **kwargs):
-        init = cls._init[cls]
-        if init is not None:
-            key = (cls, frozenset(
-                    inspect.getcallargs(init, None, *args, **kwargs).items()))
-        else:
-            key = cls
-
+        key = (cls, args, frozenset(kwargs.items()))
         if key not in cls._instances:
-            cls._instances[key] = super(SingletonArgs, cls).__call__(*args, **kwargs)
+            cls._instances[key] = super().__call__(*args, **kwargs)
         return cls._instances[key]
 
 
-class GridSingleton(object):
-    """ Class based on Singleton type to work with MongoDB connections
-    """
-    __metaclass__ = SingletonArgs
+class GridSingleton(metaclass=SingletonArgs):
+    __slots__ = ['grid']
 
-    def __init__(self, grid_class, arg1=None):
-        self.grid = grid_class(arg1)
+    def __init__(self, grid_class: Type, *args):
+        self.grid = grid_class(*args)
 
 
-static_grids = dict()
-
-
-class NamedGridRegistry:
-    """
-    Just stores a lookup table basically between instantiated grid objects and their names. Stupid.
-    """
-    @classmethod
-    def register(cls, grid_name, grid_path):
-        static_grids[grid_name] = grid_path
+class NamedFileGridRegistry:
+    _grids: Dict[str, str] = {}
 
     @classmethod
-    def get(cls, grid_name):
-        return static_grids[grid_name]
+    def register(cls, grid_name: str, grid_path: str) -> None:
+        """Register a named grid with its file path."""
+        cls._grids[grid_name] = grid_path
+
+    @classmethod
+    def get(cls, grid_name: str) -> str:
+        """Retrieve the file path for a registered grid."""
+        if grid_name not in cls._grids:
+            raise KeyError(f"Grid '{grid_name}' is not registered.")
+        return cls._grids[grid_name]
 
 
 class NamedFileGrid:
-    def __new__(cls, grid_name):
-        grid_path = NamedGridRegistry.get(grid_name)
+    def __new__(cls, grid_name: str):
+        grid_path = NamedFileGridRegistry.get(grid_name)
         return load_grid(grid_path)
 
 
@@ -102,8 +81,8 @@ class GridRegistry:
                 grid_type = name
                 args = tuple(args)
 
-        grid_func = self._registry.get(grid_type)
-        if grid_func is None:
+        grid_class = self._registry.get(grid_type)
+        if grid_class is None:
             raise KeyError(f"Grid {grid_name} is not registered.")
 
-        return GridSingleton(grid_func, *args).grid
+        return GridSingleton(grid_class, *args).grid
