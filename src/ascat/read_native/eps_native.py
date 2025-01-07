4b92625faa8fd38086031202c2269e2ea84005e9
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2025-01-07 14:18:48 +0000

    fix dtype issue

--- src/ascat/read_native/eps_native.py
+++ src/ascat/read_native/eps_native.py
@@ -1975,7 +1975,7 @@ def read_szf_fmv_13(eps_file, ignore_noise_ool=False):
 
     fields = [("longitude_full", long_nan), ("latitude_full", long_nan),
               ("sigma0_full", long_nan), ("inc_angle_full", uint_nan),
-              ("azi_angle_full", int_nan), ("flagfield", int_nan)]
+              ("azi_angle_full", int_nan), ("flagfield", uint_nan)]
 
     for f, nan_val in fields:
         data[f] = eps_file.mdr[f.upper()].flatten()
