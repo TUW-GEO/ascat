8362ae623c0ef83b6d2a964d9d11de53b845ee4f
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2025-03-29 11:17:45 +0100

    testing idw resampling

---
+++ src/ascat/resample/interface.py
@@ -0,0 +1,254 @@
+# Copyright (c) 2024, TU Wien
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of TU Wien, Department of Geodesy and Geoinformation
+#      nor the names of its contributors may be used to endorse or promote
+#      products derived from this software without specific prior written
+#      permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL TU WIEN DEPARTMENT OF GEODESY AND
+# GEOINFORMATION BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import argparse
+from pathlib import Path
+
+import numpy as np
+import xarray as xr
+from pyresample import kd_tree, SwathDefinition
+
+from ascat.utils import dtype_to_nan
+from ascat.read_native.xarray_io import get_swath_product_id
+from ascat.read_native.xarray_io import swath_io_catalog
+from ascat.regrid.regrid import regrid_swath_ds
+from ascat.regrid.regrid import retrieve_or_store_grid_lut
+
+
+def parse_args_swath_regrid(args):
+    """
+    Parse command line arguments for regridding an ASCAT swath file
+    to a regular grid.
+
+    Parameters
+    ----------
+    args : list
+        Command line arguments.
+
+    Returns
+    -------
+    parser : ArgumentParser
+        Argument Parser object.
+    """
+    parser = argparse.ArgumentParser(
+        description="Regrid an ASCAT swath file to a regular grid")
+    parser.add_argument(
+        "filepath", metavar="FILEPATH", help="Path to file or folder")
+    parser.add_argument(
+        "outpath", metavar="OUTPATH", help="Path to the output data")
+    parser.add_argument(
+        "regrid_deg",
+        metavar="REGRID_DEG",
+        type=float,
+        help="Target grid spacing in degrees")
+    parser.add_argument(
+        "--grid_store",
+        metavar="GRID_STORE",
+        help="Path for storing/loading lookup tables")
+    parser.add_argument(
+        "--suffix",
+        metavar="SUFFIX",
+        help="File suffix (default: _REGRID_DEGdeg)")
+
+    return parser.parse_args(args)
+
+
+def swath_regrid_main(cli_args):
+    """
+    Regrid an ASCAT swath file or directory of swath files
+    to a regular grid and write the results to disk.
+
+    Parameters
+    ----------
+    cli_args : list
+        Command line arguments.
+    """
+    args = parse_args_swath_regrid(cli_args)
+    filepath = Path(args.filepath)
+    trg_grid_size = args.regrid_deg
+
+    outpath = Path(args.outpath)
+    outpath.parent.mkdir(parents=True, exist_ok=True)
+
+    if args.suffix:
+        suffix = args.suffix
+    else:
+        suffix = f"_{args.regrid_deg}deg"
+
+    if filepath.is_dir():
+        files = list(filepath.glob("**/*.nc"))
+    elif filepath.is_file() and filepath.suffix == ".nc":
+        files = [filepath]
+    else:
+        raise RuntimeError("No files found at the provided filepath")
+
+    first_file = files[0]
+
+    product_id = get_swath_product_id(str(first_file.name))
+
+    if product_id is None:
+        raise RuntimeError("Product identifier unknown")
+
+    product = swath_io_catalog[product_id]
+    src_grid = product.grid
+    src_grid_size = product.grid_sampling_km
+
+    src_grid_id = f"fib_grid_{src_grid_size}km"
+    trg_grid_id = f"reg_grid_{trg_grid_size}deg"
+
+    trg_grid, grid_lut = retrieve_or_store_grid_lut(src_grid, src_grid_id,
+                                                    trg_grid_id, trg_grid_size,
+                                                    args.grid_store)
+
+    # latitude/longitude
+
+
+    # fields to weights
+    # --------
+    # surface soil moisture
+    # surface soil moisture noise
+    # surface soil moisture sensitivity
+    # backscatter40
+    # slope40
+    # curvature40
+
+    # majority
+    # --------
+    # processing_flag
+    # correction_flag
+    # backscatter40_flag
+
+    # snow cover probability
+    # frozen soil probability
+    # subsurface_scattering_prob
+    # wetland_fraction
+    # topographic_complexity
+    # surface_flag
+    # surface_flag_source
+
+    # closest
+    # --------
+    # time
+    # as_des_pass
+    # swath_indicator
+
+
+    target_def = SwathDefinition(lons=trg_grid.arrlon, lats=trg_grid.arrlat)
+
+    for f in files:
+        outfile = outpath / Path(f.stem + suffix + f.suffix)
+
+        with xr.open_dataset(f, decode_cf=False, mask_and_scale=False) as ds:
+
+            field = "surface_soil_moisture"
+            valid = ds[field] != ds[field]._FillValue
+
+            lons = ds["longitude"] * ds["longitude"].scale_factor
+            lats = ds["latitude"] * ds["latitude"].scale_factor
+
+            swath_def = SwathDefinition(lons=lons[valid], lats=lats[valid])
+            valid = ds[field] != ds[field]._FillValue
+            data = ds[field].data[valid]
+            fill_value = ds[field]._FillValue
+            output_shape = trg_grid.shape
+
+            radius = 10000.
+            k = 6
+            valid_input_index, valid_output_index, index_array, distance_array = \
+                       kd_tree.get_neighbour_info(swath_def, target_def,
+                                                  radius, neighbours=k)
+
+            weight_funcs = lambda r: 1 / r**2
+            result = kd_tree.get_sample_from_neighbour_info(
+                "custom",
+                output_shape,
+                data,
+                valid_input_index,
+                valid_output_index,
+                index_array,
+                distance_array=distance_array,
+                weight_funcs=weight_funcs,
+                fill_value=fill_value,
+                with_uncert=False)
+
+            coords = {
+                "latitude": np.int32(trg_grid.lat2d[:, 0] / 1e-6),
+                "longitude": np.int32(trg_grid.lon2d[0] / 1e-6)
+            }
+
+            regrid_ds = xr.Dataset(coords=coords)
+            regrid_ds.attrs = ds.attrs
+
+            regrid_ds["latitude"].attrs = ds["latitude"].attrs
+            regrid_ds["longitude"].attrs = ds["longitude"].attrs
+            dim = ("latitude", "longitude")
+
+            for var in ds.variables:
+                if var != field:
+                    continue
+
+                if var in ["latitude", "longitude"]:
+                    continue
+
+                if ds[var].size == 1:
+                    continue
+
+                regrid_ds[var] = (dim, result)
+                regrid_ds[var].attrs = ds[var].attrs
+                regrid_ds[var].encoding = {"zlib": True, "complevel": 4}
+
+
+            var = "location_id"
+            regrid_ds[var] = (dim, result)
+            regrid_ds[var].attrs = ds[var].attrs
+            regrid_ds[var].encoding = {"zlib": True, "complevel": 4}
+
+            # regrid_ds = regrid_swath_ds(ds, src_grid, trg_grid, grid_lut)
+            regrid_ds.to_netcdf(outfile)
+
+
+def run_swath_regrid():
+    """Run command line interface for temporal aggregation of ASCAT data."""
+    swath_regrid_main(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    filename = "/data-write/USERS/shahn/ascat_test_data/W_IT-HSAF-ROME,SAT,SSM-ASCAT-METOPC-12.5km-H121_C_LIIB_20241022071120_20240601140000_20240601145959____.nc"
+
+    outpath = "/data-write/USERS/shahn/ascat_test_data_output/"
+
+    cli_args = [
+        filename, outpath, "0.1", "--grid_store", "/home/shahn/Downloads"
+    ]
+
+    cli_args = [
+        filename,
+        outpath,
+        "0.1",
+    ]
+    swath_regrid_main(cli_args)
