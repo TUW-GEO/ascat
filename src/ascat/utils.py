9419d27c750244a114496e10cc6e842d338bc0b2
Clay Harrison <clay.harrison@geo.tuwien.ac.at>
2026-02-10 10:11:22 +0100

    Add dtype for <U3

--- src/ascat/utils.py
+++ src/ascat/utils.py
@@ -61,6 +61,7 @@ dtype_to_nan = {
     np.dtype('float32'): float32_nan,
     np.dtype('float64'): float64_nan,
     np.dtype('<U1'): None,
+    np.dtype('<U3'): None,
     np.dtype('O'): None,
     np.dtype('<M8[ns]'): np.datetime64('NaT'),
 }
